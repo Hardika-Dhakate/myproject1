0a935c7238f8f71c8bf5c54d94b9feae3785c755
Hardika Dhakate <dhakatehardika@gmail.com>
2026-01-01 22:22:21 +0530

    Add login and signup pages

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render,redirect
 
 def home(request):
-    return HttpResponse("Hello Django")
+    if 'username' not in request.session:
+        return redirect("login")
+    return render(request, "home.html", {"username": request.session['username']})
